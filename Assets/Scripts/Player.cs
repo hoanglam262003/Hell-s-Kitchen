e39ad7c653e4e5c02df3d624349f6f51544dad1b
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-11-01 16:35:36 +0700

    add collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
     private float moveSpeed = 7f;
     [SerializeField]
     private float rotationSpeed = 10f;
+    private float playerRadius = 0.7f;
+    private float playerHeight = 2f;
+    private float moveDistance;
     private bool isWalking;
 
     private void Update()
@@ -31,8 +34,31 @@ public class Player : MonoBehaviour
         }
 
         input = input.normalized;
+        moveDistance = moveSpeed * Time.deltaTime;
         Vector3 moveDirection = new Vector3(input.x, 0f, input.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+            if (canMove)
+            {
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0f, 0f, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+                if (canMove)
+                {
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    moveDirection = Vector3.zero;
+                }
+            }
+        }
         isWalking = moveDirection != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
     }
