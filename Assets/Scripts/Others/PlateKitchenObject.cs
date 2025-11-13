7cd58020d82ec9ea24ffd5f2ea3cfd490ecc2145
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-11-13 16:50:35 +0700

    Plate Icon UI

--- Assets/Scripts/Others/PlateKitchenObject.cs
+++ Assets/Scripts/Others/PlateKitchenObject.cs
@@ -35,4 +35,8 @@ public class PlateKitchenObject : KitchenObject
             return true;
         }
     }
+    public List<KitchenObjectSO> GetKitchenObjectsList()
+    {
+        return kitchenObjectsList;
+    }
 }
