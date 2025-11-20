a1d8a8e167097e46e44b19007268b634a658ca64
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-11-20 17:32:18 +0700

    main menu UI and loading UI

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button playButton;
+    [SerializeField]
+    private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.LoadScene(Loader.Scene.SampleScene);
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+
+}
