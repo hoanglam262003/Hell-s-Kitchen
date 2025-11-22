a2a082af366d5a57a05a79950c4a94923a3a1404
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-11-22 17:33:28 +0700

    music and sound manager

--- Assets/Scripts/Counters/StoveCounterSound.cs
+++ Assets/Scripts/Counters/StoveCounterSound.cs
@@ -14,6 +14,18 @@ public class StoveCounterSound : MonoBehaviour
     private void Start()
     {
         stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+        SoundManager.Instance.OnVolumeChanged += SoundManager_OnVolumeChanged;
+        audioSource.volume = SoundManager.Instance.GetVolume();
+    }
+
+    private void OnDestroy()
+    {
+        SoundManager.Instance.OnVolumeChanged -= SoundManager_OnVolumeChanged;
+    }
+
+    private void SoundManager_OnVolumeChanged(object sender, EventArgs e)
+    {
+        audioSource.volume = SoundManager.Instance.GetVolume();
     }
 
     private void StoveCounter_OnStateChanged(object sender, StoveCounter.StateChangedEventArgs e)
--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_SOUND_VOLUME = "SoundEffectsVolume";
+
+    public event EventHandler OnVolumeChanged;
     public static SoundManager Instance { get; private set; }
     [SerializeField]
     private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
+
     private void Awake()
     {
         Instance = this;
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_VOLUME, 1f);
     }
     private void Start()
     {
@@ -60,12 +66,27 @@ public class SoundManager : MonoBehaviour
     {
         PlaySound(audioClipArray[UnityEngine.Random.Range(0, audioClipArray.Length)], position, volume);
     }
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
     public void PlayFootstepSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstepAudioClips, position, volume);
+    }
+    public void ChangeVolume()
+    {
+        volume += 0.1f;
+        if (volume > 1.01f)
+        {
+            volume = 0f;
+        }
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_VOLUME, volume);
+        PlayerPrefs.Save();
+        OnVolumeChanged?.Invoke(this, EventArgs.Empty);
+    }
+    public float GetVolume()
+    {
+        return volume;
     }
 }
