3958d629b086b5196cdc4adf31fb6d0024566529
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-12-04 17:11:51 +0700

    sync pick up objects

---
+++ Assets/Scripts/Others/KitchenGameMultiplayer.cs
@@ -0,0 +1,38 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class KitchenGameMultiplayer : NetworkBehaviour
+{
+    public static KitchenGameMultiplayer Instance { get; private set; }
+
+    [SerializeField] private KitchenObjectListSO kitchenObjectListSO;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
+    {
+        SpawnKitchenObjectServerRpc(GetKitchenObjectSOIndex(kitchenObjectSO), kitchenObjectParent.GetNetworkObject());
+    }
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    private void SpawnKitchenObjectServerRpc(int kitchenObjectSOIndex, NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        KitchenObjectSO kitchenObjectSO = GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        NetworkObject kitchenObjectNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
+        kitchenObjectNetworkObject.Spawn(true);
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+    }
+    private int GetKitchenObjectSOIndex(KitchenObjectSO kitchenObjectSO)
+    {
+        return kitchenObjectListSO.kitchenObjectSOList.IndexOf(kitchenObjectSO);
+    }
+    private KitchenObjectSO GetKitchenObjectSOFromIndex(int index)
+    {
+        return kitchenObjectListSO.kitchenObjectSOList[index];
+    }
+}
