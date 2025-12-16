6950d691730abc4a9926063b66ad703889d143d8
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-12-16 19:43:54 +0700

    connection scene flow

---
+++ Assets/Scripts/Others/CharacterSelectReady.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CharacterSelectReady : NetworkBehaviour
+{
+    public static CharacterSelectReady Instance { get; private set; }
+
+    private Dictionary<ulong, bool> playerReadyDictionary;
+
+    private void Awake()
+    {
+        Instance = this;
+        playerReadyDictionary = new Dictionary<ulong, bool>();
+    }
+
+    public void SetPlayerReady()
+    {
+        SetPlayerReadyServerRpc();
+    }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    private void SetPlayerReadyServerRpc(RpcParams rpcParams = default)
+    {
+        playerReadyDictionary[rpcParams.Receive.SenderClientId] = true;
+
+        bool allReady = true;
+
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            if (!playerReadyDictionary.ContainsKey(clientId) || !playerReadyDictionary[clientId])
+            {
+                allReady = false;
+                break;
+            }
+        }
+
+        if (allReady)
+        {
+            Loader.LoadNetwork(Loader.Scene.SampleScene);
+        }
+
+        Debug.Log($"All players ready: {allReady}");
+    }
+
+}
---
+++ Assets/Scripts/UI/TestingCharacterSelectUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingCharacterSelectUI : MonoBehaviour
+{
+    [SerializeField] private Button readyButton;
+
+    private void Awake()
+    {
+        readyButton.onClick.AddListener(() =>
+        {
+            CharacterSelectReady.Instance.SetPlayerReady();
+            readyButton.interactable = false;
+        });
+    }
+}
