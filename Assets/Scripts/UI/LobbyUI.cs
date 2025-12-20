a6fa817be423a43047d67414e0038ed9b2929124
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-12-20 17:59:57 +0700

    lobby function

---
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUI : MonoBehaviour
+{
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button createLobbyButton;
+    [SerializeField] private Button quickJoinButton;
+    [SerializeField] private Button joinCodeButton;
+    [SerializeField] private TMP_InputField joinCodeInputField;
+    [SerializeField] private LobbyCreateUI lobbyCreateUI;
+
+    private void Awake()
+    {
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            Loader.LoadScene(Loader.Scene.MainMenuScene);
+        });
+        createLobbyButton.onClick.AddListener(() =>
+        {
+            lobbyCreateUI.Show();
+        });
+        quickJoinButton.onClick.AddListener(() =>
+        {
+            KitchenGameLobby.Instance.QuickJoin();
+        });
+        joinCodeButton.onClick.AddListener(() =>
+        {
+            string joinCode = joinCodeInputField.text;
+            KitchenGameLobby.Instance.JoinWithCode(joinCode);
+        });
+    }
+}
